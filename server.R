87991552fd98e976c70dd0955639c8e7e51123c8
Kevin Tran <ktran@tack.tech>
2017-02-28 20:58:52 -0800

    set up data

---
+++ server.R
@@ -0,0 +1,17 @@
+#install.packages("leaflet")
+library(leaflet)
+library(dplyr)
+#setwd("~/Dropbox/Classes/INFO201/Project/final_project")
+
+
+collision.data <- read.csv("./SDOT_Collisions.csv", stringsAsFactors = FALSE)
+
+coordinates <- collision.data$Shape
+
+lat <- sapply(strsplit(coordinates, split=", "), "[", 1)
+lng <- sapply(strsplit(coordinates, split=", "), "[", 2)
+
+lat <- gsub("\\(", "", lat)
+lng <- gsub(")", "", lng)
+
+collision.data <- mutate(collision.data, "Latitude" = as.numeric(lat), "Longitude" = as.numeric(lng))
