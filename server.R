0c46847b7ac54a77ee80fd9cafc4c76b699f81c3
Brendon Zhang <bzb4565@cs.washington.edu>
2017-03-06 10:11:49 -0800

    optimized filtering and speed up rendering via only generating circle markers for particular city zoomed on

--- server.R
+++ server.R
@@ -9,42 +9,68 @@ shinyServer(function(input, output)  {
     
     map <- leaflet() %>% 
       addTiles() %>% 
-      addCircles(neighborhood.lng, neighborhood.lat, count, neighborhood, "Overview", FALSE, fillOpacity = 0.5) %>%
-      addCircleMarkers(~Longitude, ~Latitude, 4, NULL, "Ballard", FALSE, data = ballard.disp(), fillOpacity = 0.3) %>% 
-      hideGroup("Ballard") %>% 
-      addCircleMarkers(~Longitude, ~Latitude, 4, NULL, "Phinney Ridge", FALSE, data = phinney.ridge.disp(), fillOpacity = 0.3) %>%  
-      hideGroup("Phinney Ridge") %>% 
-      addCircleMarkers(~Longitude, ~Latitude, 4, NULL, "Fremont", FALSE, data = fremont.disp(), fillOpacity = 0.3) %>% 
-      hideGroup("Fremont") %>% 
-      addCircleMarkers(~Longitude, ~Latitude, 4, NULL, "Greenwood", FALSE, data = greenwood.disp(), fillOpacity = 0.3) %>% 
-      hideGroup("Greenwood") %>% 
-      addCircleMarkers(~Longitude, ~Latitude, 4, NULL, "University District", FALSE, data = university.district.disp(), fillOpacity = 0.3) %>% 
-      hideGroup("University District") %>% 
-      addCircleMarkers(~Longitude, ~Latitude, 4, NULL, "Green Lake", FALSE, data = green.lake.disp(), fillOpacity = 0.3) %>% 
-      hideGroup("Green Lake") %>% 
-      addCircleMarkers(~Longitude, ~Latitude, 4, NULL, "Northgate", FALSE, data = northgate.disp(), fillOpacity = 0.3) %>% 
-      hideGroup("Northgate") %>% 
-      addCircleMarkers(~Longitude, ~Latitude, 4, NULL, "Magnolia", FALSE, data = magnolia.disp(), fillOpacity = 0.3) %>%  
-      hideGroup("Magnolia") %>% 
-      addCircleMarkers(~Longitude, ~Latitude, 4, NULL, "Queen Anne", FALSE, data = queen.anne.disp(), fillOpacity = 0.3) %>%  
-      hideGroup("Queen Anne") %>% 
-      addCircleMarkers(~Longitude, ~Latitude, 4, NULL, "Capitol Hill", FALSE, data = capitol.hill.disp(), fillOpacity = 0.3) %>% 
-      hideGroup("Capitol Hill") %>% 
+      addCircles(neighborhood.lng, neighborhood.lat, 
+                 c(nrow(ballard.disp())/5, nrow(phinney.ridge.disp())/3, nrow(fremont.disp())/3, nrow(greenwood.disp())/3, 
+                   nrow(university.district.disp())/4, nrow(green.lake.disp())/3, nrow(northgate.disp())/4, nrow(magnolia.disp()),
+                   nrow(queen.anne.disp()), nrow(capitol.hill.disp()))
+                 , neighborhood, "Overview", FALSE, fillOpacity = 0.5) %>%
+      #addCircleMarkers(~Longitude, ~Latitude, 4, NULL, "Ballard", FALSE, data = ballard.disp(), fillOpacity = 0.3) %>% 
+      #hideGroup("Ballard") %>% 
+      #addCircleMarkers(~Longitude, ~Latitude, 4, NULL, "Phinney Ridge", FALSE, data = phinney.ridge.disp(), fillOpacity = 0.3) %>%  
+      #hideGroup("Phinney Ridge") %>% 
+      #addCircleMarkers(~Longitude, ~Latitude, 4, NULL, "Fremont", FALSE, data = fremont.disp(), fillOpacity = 0.3) %>% 
+      #hideGroup("Fremont") %>% 
+      #addCircleMarkers(~Longitude, ~Latitude, 4, NULL, "Greenwood", FALSE, data = greenwood.disp(), fillOpacity = 0.3) %>% 
+      #hideGroup("Greenwood") %>% 
+      #addCircleMarkers(~Longitude, ~Latitude, 4, NULL, "University District", FALSE, data = university.district.disp(), fillOpacity = 0.3) %>% 
+      #hideGroup("University District") %>% 
+      #addCircleMarkers(~Longitude, ~Latitude, 4, NULL, "Green Lake", FALSE, data = green.lake.disp(), fillOpacity = 0.3) %>% 
+      #hideGroup("Green Lake") %>% 
+      #addCircleMarkers(~Longitude, ~Latitude, 4, NULL, "Northgate", FALSE, data = northgate.disp(), fillOpacity = 0.3) %>% 
+      #hideGroup("Northgate") %>% 
+      #addCircleMarkers(~Longitude, ~Latitude, 4, NULL, "Magnolia", FALSE, data = magnolia.disp(), fillOpacity = 0.3) %>%  
+      #hideGroup("Magnolia") %>% 
+      #addCircleMarkers(~Longitude, ~Latitude, 4, NULL, "Queen Anne", FALSE, data = queen.anne.disp(), fillOpacity = 0.3) %>%  
+      #hideGroup("Queen Anne") %>% 
+      #addCircleMarkers(~Longitude, ~Latitude, 4, NULL, "Capitol Hill", FALSE, data = capitol.hill.disp(), fillOpacity = 0.3) %>% 
+      #hideGroup("Capitol Hill") %>% 
       setView(-122.339220, 47.677622, 12)
     
-    current.group <- "Overview"
+    current.group <- "placeholder"
     
     if(!is.null(clicks$map.click) & is.null(clicks$shape.click)) {
-      map <- map %>% hideGroup(current.group) %>% showGroup("Overview") %>%
+      #current.group
+      #map <- map %>% hideGroup("Ballard") %>% showGroup("Overview") %>%
+      #  setView(-122.339220, 47.677622, 12)
+      #map.removeLayer(current.group)
+      #removeMarker(map, )
+      removeMarker(map, current.group)
+      map <- map %>% showGroup("Overview") %>% 
         setView(-122.339220, 47.677622, 12)
     } else if(!is.null(clicks$shape.click) & is.null(clicks$map.click)) {
       current.group <- clicks$shape.click$id
-      map <- map %>% hideGroup("Overview") %>% showGroup(current.group) %>% 
-        setView(lat = clicks$shape.click$lat, lng = clicks$shape.click$lng, zoom = 14)
+      
+      print(current.group)
+      #map <- map %>% hideGroup("Overview") %>% showGroup("Ballard") %>% 
+        #setView(lat = clicks$shape.click$lat, lng = clicks$shape.click$lng, zoom = 14)
+      #map.removeLayer("Overview")
+      #print(getLayer())
+      
+      print(gsub(" ", ".", tolower(current.group)))
+      
+      data.res <- eval(parse(text = paste0(gsub(" ", ".", tolower(current.group)), ".disp()")))
+      
+      map <- map %>% hideGroup("Overview") %>% addCircleMarkers(~Longitude, ~Latitude, 4, NULL, current.group, FALSE, data = data.res, fillOpacity = 0.3) %>% 
+          setView(lat = clicks$shape.click$lat, lng = clicks$shape.click$lng, zoom = 14) %>% showGroup(current.group)
+      #map <- map %>% showGroup(current.group) %>% hideGroup("Overview") %>% 
+        #setView(lat = clicks$shape.click$lat, lng = clicks$shape.click$lng, zoom = 14)
     }
     
     return(map)
   })
+  
+  
+  
   
   fremont.disp <- reactive({return(eval(parse(text = gen.string2("fremont"))))})
   phinney.ridge.disp <- reactive({return(eval(parse(text = gen.string2("phinney.ridge"))))})
@@ -56,6 +82,7 @@ shinyServer(function(input, output)  {
   magnolia.disp <- reactive({return(eval(parse(text = gen.string2("magnolia"))))})
   northgate.disp <- reactive({return(eval(parse(text = gen.string2("northgate"))))})
   capitol.hill.disp <- reactive({return(eval(parse(text = gen.string2("capitol.hill"))))})
+
   
   gen.string2 <- function(city){
     #roadcond 
@@ -85,10 +112,22 @@ shinyServer(function(input, output)  {
     #
     #allowing users to set variables as "YES/NO/OFF" will be kind of annoying.... sigh.
     
-    str <- "collision.data"
-    str <- paste(str, "%>%", sprintf("filter(Longitude < %1$s.limits$upper.lng & Longitude > %1$s.limits$lower.lng)", city))
-    str <- paste(str, "%>%", sprintf("filter(Latitude < %1$s.limits$upper.lat & Latitude > %1$s.limits$lower.lat)", city))
-    if(input$raining){str <- paste(str, "%>%", "filter(WEATHER == 'Raining')")}
+    #overview.count <- c(nrow(ballard.disp())/5, nrow(phinney.ridge.disp())/3, nrow(fremont.disp())/3, nrow(greenwood.disp())/3, 
+                        #nrow(university.district.disp())/4, nrow(green.lake.disp())/3, nrow(northgate.disp())/4, nrow(magnolia.disp()),
+                        #nrow(queen.anne.disp()), nrow(capitol.hill.disp()))
+    
+    str <- sprintf("%1$s.data", city)
+    #str <- paste(str, "%>%", sprintf("filter(Longitude < %1$s.limits$upper.lng & Longitude > %1$s.limits$lower.lng)", city))
+    #str <- paste(str, "%>%", sprintf("filter(Latitude < %1$s.limits$upper.lat & Latitude > %1$s.limits$lower.lat)", city))
+    #if(input$raining){str <- paste(str, "%>%", "filter(WEATHER == 'Raining')")}
+    str <- paste(str, "%>%", "filter(TRUE")
+    if(input$weather != "Off"){str <- paste(str, "& WEATHER == input$weather")}
+    if(input$roadcond != "Off"){str <- paste(str, "& ROADCOND == input$roadcond")}
+    if(input$lightcond != "Off"){str <- paste(str, "& LIGHTCOND == input$lightcond")}
+    str <- paste(str, ")")
+    #if(input$weather != "Off"){str <- paste(str, "%>%", "filter(WEATHER == input$weather)")}
+    #if(input$roadcond != "Off"){str <- paste(str, "%>%", "filter(ROADCOND == input$roadcond)")}
+    #if(input$lightcond != "Off"){str <- paste(str, "%>%", "filter(LIGHTCOND == input$lightcond)")}
     
     return(str)
   }
--- ui.R
+++ ui.R
@@ -15,7 +15,22 @@ ui <- fluidPage(
                     FALSE),
       checkboxInput("raining",
                     "Check for rain",
-                    FALSE)
+                    FALSE),
+      h3("toggles:"),
+      selectInput("roadcond",
+                  "road condition",
+                  choices = c("Off", "Dry", "Wet", "Snow/Slush", "Unknown", "Ice", "Sand/Mud/Dirt", "Other", "Oil", "Standing Water")
+      ),
+      selectInput("weather",
+                  "weather",
+                  choices = c("Off", "Clear or Partly Cloudy", "Overcast", "Raining", "Unknown", "Fog/Smog/Smoke", "Snowing", "Other", 
+                              "Sleet/Hail/Freezing Rain", "Severe Crosswind", "Blowing Sand or Dirt or Snow")
+      ),
+      selectInput("lightcond",
+                  "light condition",
+                  choices = c("Off", "DayLight", "Dark - Street Lights On", "Dusk", 
+                              "Dark - Street Lights Off", "Unknown", "Dawn", "Dark - No Street Lights", "Other")
+      )
     ),
     mainPanel(
       tabsetPanel(type = "tabs",
