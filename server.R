3c1779b46b076bff829f7a6c4c083955a0259fad
Kevin Tran <ktran@tack.tech>
2017-03-05 17:54:23 -0800

    changed northgate neighborhood to maple leaf

--- server.R
+++ server.R
@@ -9,7 +9,7 @@ shinyServer(function(input, output)  {
     
     map <- leaflet() %>% 
       addTiles() %>% 
-      addCircles(neighborhood.lng, neighborhood.lat, count, neighborhood, "Overview", FALSE, fillOpacity = 0.5) %>%
+      addCircles(neighborhood.lng, neighborhood.lat, size, neighborhood, "Overview", FALSE, fillOpacity = 0.5) %>%
       addCircleMarkers(~Longitude, ~Latitude, 4, NULL, "Ballard", FALSE, data = ballard.data, fillOpacity = 0.3) %>% 
       hideGroup("Ballard") %>% 
       addCircleMarkers(~Longitude, ~Latitude, 4, NULL, "Phinney Ridge", FALSE, data = phinney.ridge.data, fillOpacity = 0.3) %>%  
@@ -22,21 +22,21 @@ shinyServer(function(input, output)  {
       hideGroup("University District") %>% 
       addCircleMarkers(~Longitude, ~Latitude, 4, NULL, "Green Lake", FALSE, data = green.lake.data, fillOpacity = 0.3) %>% 
       hideGroup("Green Lake") %>% 
-      addCircleMarkers(~Longitude, ~Latitude, 4, NULL, "Northgate", FALSE, data = northgate.data, fillOpacity = 0.3) %>% 
-      hideGroup("Northgate") %>% 
+      addCircleMarkers(~Longitude, ~Latitude, 4, NULL, "Maple Leaf", FALSE, data = maple.leaf.data, fillOpacity = 0.3) %>% 
+      hideGroup("Maple Leaf") %>% 
       addCircleMarkers(~Longitude, ~Latitude, 4, NULL, "Magnolia", FALSE, data = magnolia.data, fillOpacity = 0.3) %>%  
       hideGroup("Magnolia") %>% 
       addCircleMarkers(~Longitude, ~Latitude, 4, NULL, "Queen Anne", FALSE, data = queen.anne.data, fillOpacity = 0.3) %>%  
       hideGroup("Queen Anne") %>% 
       addCircleMarkers(~Longitude, ~Latitude, 4, NULL, "Capitol Hill", FALSE, data = capitol.hill.data, fillOpacity = 0.3) %>% 
       hideGroup("Capitol Hill") %>% 
-      setView(-122.339220, 47.677622, 12)
+      setView(-122.340098, 47.665702, 12)
     
     current.group <- "Overview"
     
     if(!is.null(clicks$map.click) & is.null(clicks$shape.click)) {
       map <- map %>% hideGroup(current.group) %>% showGroup("Overview") %>%
-        setView(-122.339220, 47.677622, 12)
+        setView(-122.340098, 47.665702, 12)
     } else if(!is.null(clicks$shape.click) & is.null(clicks$map.click)) {
       current.group <- clicks$shape.click$id
       map <- map %>% hideGroup("Overview") %>% showGroup(current.group) %>% 
@@ -59,6 +59,7 @@ shinyServer(function(input, output)  {
   })
   
   showCollisionInfo <- function(collision, lat, lng) {
+    print(collision)
     selectedCollisions <- filter(collision.data, Latitude == lat, Longitude == lng)
     selectedCollision <- sample_n(selectedCollisions, 1)
     content <- as.character(tagList(
@@ -67,15 +68,17 @@ shinyServer(function(input, output)  {
       tags$strong("Collision Type: "), tags$em(selectedCollision$COLLISIONTYPE), tags$br(),
       tags$strong("SDOT description: "), tags$em(selectedCollision$SDOT_COLDESC), tags$br(),
       tags$strong("Number of people: "), tags$em(selectedCollision$PERSONCOUNT), tags$br(),
-      tags$strong("Number of injuries: "), tags$em(selectedCollision$INJURIES), tags$br(),
-      tags$strong("Distance: "), tags$em(selectedCollision$DISTANCE)
+      tags$strong("Number of injuries: "), tags$em(selectedCollision$INJURIES)
     ))
     leafletProxy("map") %>% addPopups(lng, lat, content)
   }
   
   observe({
+    click <- NULL
+    hover <- NULL
     leafletProxy("map") %>% clearPopups()
     click <- input$map_marker_click
+    print(hover)
     if (!is.null(click)) {
       isolate({
         showCollisionInfo(click$id, click$lat, click$lng)
